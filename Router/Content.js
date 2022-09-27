a968fbe4ef5e1937f6cac1bd335ba62e9c334327
mikye912 <mikye912@gmail.com>
2022-09-27 17:57:46 +0900

    kmk_20220927

--- Router/Content.js
+++ Router/Content.js
@@ -3,35 +3,6 @@ const common = require('$Common/common');
 const express = require('express');
 const router = express.Router();
 
-// router.route('/items')
-//     .get((req, res) => {
-//         let obj = new Object();
-
-//         obj.uInfo = common.reqTokenToUinfo(req.headers.x_auth);
-
-//         const getSearch = async () => {
-//             let arr = new Array();
-//             arr = [...arr, dbconn.createPromise('$Main/Content/getUserSearch', obj)];
-//             arr = [...arr, dbconn.createPromise('$Main/Content/getUserDepart', obj)];
-//             arr = [...arr, dbconn.createPromise('$Main/Content/getUserTid', obj)];
-//             arr = [...arr, dbconn.createPromise('$Main/Content/getUserAcq')];
-//             arr = [...arr, dbconn.createPromise('$Main/Content/getUserDomain', obj)];
-
-//             return await dbconn.getDataAll(arr)
-//             .then((res) => {
-//                 return {
-//                    uSearch: common.uSearch_trans(res[0], res[1], res[2], res[3]),
-//                    uDomain: res[4],
-//                 }
-//             });
-//         }
-//         getSearch()
-//         .then(res.send.bind(res))
-//         .catch((err) => {
-//             res.status(500).send(err.toString())
-//         });
-//     })
-
 router.route('/:pages/:do')
     .get((req, res) => {
         let obj = new Object();
